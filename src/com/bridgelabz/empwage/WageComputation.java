ea57ffb767c2386a8eeeef52b191cd692ae26912
sharad2512 <shnarute95@gmail.com>
2022-12-22 08:50:34 +0530

    ADDED: code to print welcome msg

---
+++ src/com/bridgelabz/empwage/WageComputation.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.empwage;
+
+public class WageComputation {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Employee wage computation program");
+    }
+}
